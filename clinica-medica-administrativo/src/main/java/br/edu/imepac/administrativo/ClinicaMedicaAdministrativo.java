eabf4c866b9c435453741e3d51d9deb251f22cfe
midinatto <midinatto@gmail>
2025-06-26 11:37:43 -0300

    Docker compose ajustado e Dockerfile adicionado em todos os modulos

--- clinica-medica-administrativo/src/main/java/br/edu/imepac/administrativo/ClinicaMedicaAdministrativo.java
+++ clinica-medica-administrativo/src/main/java/br/edu/imepac/administrativo/ClinicaMedicaAdministrativo.java
@@ -2,7 +2,11 @@ package br.edu.imepac.administrativo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@SpringBootApplication
+
+@SpringBootApplication(scanBasePackages = {
+        "br.edu.imepac.administrativo",
+        "br.edu.imepac.central"
+})
 public class ClinicaMedicaAdministrativo {
 
     public static void main(String[] args) {
--- clinica-medica-central/src/main/java/br/edu/imepac/central/services/FuncionarioService.java
+++ clinica-medica-central/src/main/java/br/edu/imepac/central/services/FuncionarioService.java
@@ -1,4 +1,11 @@
 package br.edu.imepac.central.services;
 
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Slf4j
+@Service
 public class FuncionarioService {
 }
